5e043716bb0e84680853906bbb56fe0af4cb9aab
pipemalz <sinister012@gmail.com>
2022-12-24 17:15:40 -0500

    Creada funcion para imprimir resultados area de triangulo, circulo, rectangulo dependiendo de valor de los inputs

--- formulas.js
+++ formulas.js
@@ -12,8 +12,14 @@ const inputs = {
         base: document.getElementById('input_base_rectangulo'),
         altura: document.getElementById('input_altura_rectangulo')
     },
-    triangulo : {},
-    circulo : {}
+    triangulo : {
+        base: document.getElementById('input_base_triangulo'),
+        lado1: document.getElementById('input_lado1_triangulo'),
+        lado2: document.getElementById('input_lado2_triangulo')
+    },
+    circulo : {
+        radio: document.getElementById('input_radio_circulo')
+    }
 }
 
 for (const button in buttons) {
@@ -41,18 +47,6 @@ class Rectangulo{
         <p> <strong> Altura </strong> = ${this.altura}px </p>
         <p> <strong> Perimetro </strong> = ${this.perimetro()}px </p>
         <p> <strong> Area </strong> = ${this.area()}px </p>`;
-    }
-}
-
-function imprimirDatos(e){
-    const button = e.target.classList[1];
-    if(button == 'button--rectangulo'){
-        const base = inputs['rectangulo']['base'].value;
-        const altura = inputs['rectangulo']['altura'].value;
-        if(base != '' && altura != ''){
-            const rectangulo = new Rectangulo(base, altura);
-            rectangulo.print();
-        }
     }
 }
 
@@ -94,16 +88,47 @@ class Triangulo{
         const c = this.base;
         return Math.sqrt(s* (s-a) * (s-b) * (s-c));
     }
+    print(){
+        let area;
+        if(!this.area()){
+            area = `No es un triangulo`;
+        }else{
+            area = this.area();
+        }
+        triangulo_div.innerText = `Triángulo
+        Base = ${this.base}
+        Lado 1 = ${this.l1}
+        Lado 2 = ${this.l2}
+        Perimetro = ${this.perimetro()}
+        Area = ${area}`
+    }
 }
 
-const triangulo = new Triangulo(6,8,5);
-triangulo_div.innerText = `Triángulo
-Base = ${triangulo.base}px
-Lado 1 = ${triangulo.l1}px
-Lado 2 = ${triangulo.l2}px
-Perimetro = ${triangulo.perimetro()}px
-Area = ${triangulo.area()}px`
-
+function imprimirDatos(e){
+    const button = e.target.classList[1];
+    if(button == 'button--rectangulo'){
+        const base = inputs['rectangulo']['base'].value;
+        const altura = inputs['rectangulo']['altura'].value;        if(base != '' && altura != ''){
+            const rectangulo = new Rectangulo(parseInt(base), parseInt(altura));
+            rectangulo.print();
+        }
+    }else if(button == 'button--triangulo'){
+        const base = inputs['triangulo']['base'].value;
+        const lado1 = inputs['triangulo']['lado1'].value;
+        const lado2 = inputs['triangulo']['lado2'].value;
+        if(base != '' && lado1 != '' && lado2 != ''){
+            const triangulo = new Triangulo(parseInt(base), parseInt(lado1), parseInt(lado2));
+            triangulo.print();
+            console.log(base)
+        }
+    }else if(button == 'button--circulo'){
+        const radio = inputs['circulo']['radio'].value;
+        if(radio != ''){
+            const circulo = new Circulo(parseInt(radio));
+            circulo.print();
+        }
+    }
+}
 
 //CIRCULO 
 //DIAMETRO = RADIO * 2
@@ -117,46 +142,45 @@ class Circulo{
         this.perimetro = 2 * Math.PI  * this.radio;
         this.area = Math.PI * this.radio**2;
     }
-}
-
-const circulo = new Circulo(20);
-circulo_div.innerText = `Circulo 
-Radio = ${circulo.radio}px
-Diametro = ${circulo.diametro}px
-Perimetro = ${circulo.perimetro}px
-Area = ${circulo.area}px`
-
-
-class TrianguloIsoceles{
-    constructor(base, l){
-        this.base = base;
-        this.l = l;
-    }
-    altura(){
-        if(this.base == this.l){
-            console.log('No es un triangulo isoceles');
-        }else{
-           return Math.sqrt(this.l**2 - (this.base**2 / 4));
-        }
+    print(){
+        circulo_div.innerText = `Circulo 
+        Radio = ${this.radio}px
+        Diametro = ${this.diametro}px
+        Perimetro = ${this.perimetro}px
+        Area = ${this.area}px`
     }
 }
 
-const trianguloIsoceles = new TrianguloIsoceles(4, 6);
+// class TrianguloIsoceles{
+//     constructor(base, l){
+//         this.base = base;
+//         this.l = l;
+//     }
+//     altura(){
+//         if(this.base == this.l){
+//             console.log('No es un triangulo isoceles');
+//         }else{
+//            return Math.sqrt(this.l**2 - (this.base**2 / 4));
+//         }
+//     }
+// }
 
-class TrianguloEscaleno{
-    constructor(l1,l2,l3){
-        this.l1 = l1;
-        this.l2 = l2;
-        this.l3 = l3;
-    }
-    altura(){
-       //Semiperimetro
-        const s = (this.l1 + this.l2 + this.l3)/2;
-        const a = this.l1;
-        const b = this.l2;
-        const c = this.l3;
-        return Math.floor((2 / a) * Math.sqrt(s * (s - a) * (s - b) * (s - c)));
-    }   
-}
+// const trianguloIsoceles = new TrianguloIsoceles(4, 6);
 
-const trianguloEscaleno = new TrianguloEscaleno(16, 8, 10);
+// class TrianguloEscaleno{
+//     constructor(l1,l2,l3){
+//         this.l1 = l1;
+//         this.l2 = l2;
+//         this.l3 = l3;
+//     }
+//     altura(){
+//        //Semiperimetro
+//         const s = (this.l1 + this.l2 + this.l3)/2;
+//         const a = this.l1;
+//         const b = this.l2;
+//         const c = this.l3;
+//         return Math.floor((2 / a) * Math.sqrt(s * (s - a) * (s - b) * (s - c)));
+//     }   
+// }
+
+// const trianguloEscaleno = new TrianguloEscaleno(16, 8, 10);
